d93b1642edb24c6f4d7b0774c7704603dd5d7eb7
Edanriell <Edanriell@gmail.com>
2025-04-20 14:42:13 +0300

    feature(rich-apartment-domain-model): refactored anemic domain model to rich

---
+++ Bookify.Domain/Apartments/Currency.cs
@@ -0,0 +1,35 @@
+namespace Bookify.Domain.Apartments;
+
+// Amount and currency essentially represent money, and this is a great candidate to introduce a value object
+// because money is an important concept of the apartment booking domain.
+// We do not use only the default constructor in this case because we want more control over our currency value object
+public record Currency
+{
+	// Supported currencies by our system
+	// None currency value does not have currency code, and we aren't going to return it 
+	// from the list of all currencies because we don't want to expose it to the outside our domain project
+	// The internal keyword is allowing us to achieve that by hiding this field from the outside the domain assembly
+	internal static readonly Currency None = new("");
+	public static readonly Currency Usd = new("USD");
+	public static readonly Currency Eur = new("EUR");
+
+	// Exposing all currencies in our system
+	public static readonly IReadOnlyCollection<Currency> All = new[] { Usd, Eur };
+
+	// Private constructor
+	private Currency(string code)
+	{
+		Code = code;
+	}
+
+	public string Code { get; init; }
+
+	// This method allows us to pass in currency code and get back the currency instance
+	public static Currency FromCode(string code)
+	{
+		// We are scanning all of our currencies by using FirstOrDefault method, if currency code matches one that is passed in, we return an instance of currency
+		// If method FirstOrDefault returns null, we throw an exception "The currency code is invalid"
+		return All.FirstOrDefault(c => c.Code == code) ??
+			   throw new ApplicationException("The currency code is invalid");
+	}
+}
---
+++ Bookify.Domain/Apartments/Money.cs
@@ -0,0 +1,19 @@
+namespace Bookify.Domain.Apartments;
+
+public record Money(decimal Amount, Currency Currency)
+{
+	// Operator overloading, if we want to add to instances of Money class, we can use + operator
+	public static Money operator +(Money first, Money second)
+	{
+		// The Currency has to be equal, otherwise we can't add two money instances
+		if (first.Currency != second.Currency) throw new InvalidOperationException("Currencies have to be equal");
+
+		return new Money(first.Amount + second.Amount, first.Currency);
+	}
+
+	// Static method which allows us to create a money instance with no value
+	public static Money Zero()
+	{
+		return new Money(0, Currency.None);
+	}
+}
