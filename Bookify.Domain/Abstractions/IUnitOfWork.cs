35e45a0c5388658ccf6538c196f961a0c1942fcd
Edanriell <Edanriell@gmail.com>
2025-04-20 18:04:06 +0300

    feature(repos-unit-of-work): implemented necessary patterns

---
+++ Bookify.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+namespace Bookify.Domain.Abstractions;
+
+// The UnitOfWork and the repository pattern are necessary for defining
+// a rich domain model 
+public interface IUnitOfWork
+{
+	// Method SaveChangesAsync takes any changes that are pending in our repositories and persist
+	// it in the database. 
+	Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
