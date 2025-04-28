95c78aac9afd32f1e3c5d5fb78379feac3da31d4
Edanriell <Edanriell@gmail.com>
2025-04-28 21:56:01 +0300

    feature(result-class): implemented result and error classes

---
+++ Bookify.Domain/Abstractions/Error.cs
@@ -0,0 +1,8 @@
+namespace Bookify.Domain.Abstractions;
+
+// Error represents that something went wrong and we want to assign it a unique code and a name
+public record Error(string Code, string Name)
+{
+	public static Error None = new(string.Empty, string.Empty);
+	public static Error NullValue = new("Error.NullValue", "Null value was provided");
+}
