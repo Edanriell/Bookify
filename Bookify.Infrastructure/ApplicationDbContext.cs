e28becfb5b707e65b8093ae07f42077405fbd09d
Edanriell <Edanriell@gmail.com>
2025-05-07 10:19:32 +0300

    feature(efc-config): created basic config

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ namespace Bookify.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
+	// Constructor accepts the DBContextOption class, and it is passing it to the BaseClass constructor, accepting this type.
 	public ApplicationDbContext(DbContextOptions options) : base(options)
 	{
 	}
--- Bookify.Infrastructure/DependecyInjection.cs
+++ Bookify.Infrastructure/DependecyInjection.cs
@@ -2,6 +2,7 @@ using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -15,6 +16,27 @@ public static class DependencyInjection
 		services.AddTransient<IDateTimeProvider, DateTimeProvider>();
 
 		services.AddTransient<IEmailService, EmailService>();
+
+		// We are using the IConfiguration instance to get the value of the connection string
+		// from our application settings. This is basically the connection string that
+		// EFCore is going to use connect to our PostgreSQL database instance. 
+		var connectionString = configuration.GetConnectionString("Database") ??
+							   throw new ArgumentNullException(nameof(configuration));
+
+		// To register EFCore we need to call the AddDbContext method which is exposed by EntityFrameworkCore
+		// We specify our database context as the generic argument, and then on the DatabaseContextOptionsBuilder we 
+		// need to call the specific provider that we are using as our database, in our case it would be Postgre
+		// so we have to call the UseNpgsql method and give it the connection string so that it can connect to our
+		// database.
+		// IMPORTANT! EFCore, by convention, is going to use a title case for the names of the tables and columns in our database model.
+		// Postgre on the other hand prefers snake case. 
+		services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			// We are using EFCore.NamingConventions to set explicit snake case, and this is going to align 
+			// our table and column names with the SnakeCaseNamingConvention which is what Postgre is using and also the SQL
+			// that we added in the application project was referencing this naming convention for the columns and tables. 
+			options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+		});
 
 		return services;
 	}
