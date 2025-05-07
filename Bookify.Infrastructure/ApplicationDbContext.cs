3979620f60819055241d062b5c712e5e2e6cf774
Edanriell <Edanriell@gmail.com>
2025-05-07 19:29:09 +0300

    feature(applying-configurations): applyed configs

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -9,4 +9,17 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 	public ApplicationDbContext(DbContextOptions options) : base(options)
 	{
 	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		// We are specifying the assembly containing our database context. 
+		// When our model is being configured, it's going to scan this assembly
+		// find our entity configurations that we added in the configurations folder, and
+		// apply them to the entity framework data model. 
+		// IMPORTANT! There is no risk to forget to apply our entity configuration, because it's going
+		// to be applied automatically. 
+		modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+		base.OnModelCreating(modelBuilder);
+	}
 }
