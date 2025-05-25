0e040d303634962ad42ddc33ea938916ac5882f5
Edanriell <Edanriell@gmail.com>
2025-05-25 22:37:33 +0300

    feature(logging): implemented structured logging

--- Bookify.Api/Program.cs
+++ Bookify.Api/Program.cs
@@ -1,8 +1,26 @@
 using Bookify.Api.Extensions;
 using Bookify.Application;
 using Bookify.Infrastructure;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder (
+		args : args
+	);
+
+
+// Accessing our web application builder, and we are going to 
+// access the host property so that we can configure our host builder. 
+// After we have an extension method available that is called UseSerilog, and we are going to
+// use an overload of this method that gives us access to the host builder context and
+// the logger configuration, and what we are going to do is to configure our Serilog configuration
+// from our application settings. We are using host builder context to provide
+// the IConfiguration instance so that Serilog can be configured from the application settings.
+builder.Host.UseSerilog (
+		// Allows us to configure Serilog from appsettings.json
+		configureLogger : ( context, configuration ) => configuration.ReadFrom.Configuration (
+				configuration : context.Configuration
+			)
+	);
 
 builder.Services.AddControllers();
 
@@ -12,11 +30,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 // This method expects an IConfiguration instance, which we
 // can pass from builder configuration. 
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddInfrastructure (
+		configuration : builder.Configuration
+	);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if ( app.Environment.IsDevelopment() )
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
@@ -28,6 +48,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRequestContextLogging();
+
+// Introduce middleware that is going to hook into our incoming requests
+// and start logging useful information about the processing of our API requests,
+// such as the status codes, request times, any exceptions, and so on. 
+app.UseSerilogRequestLogging();
 
 app.UseCustomExceptionHandler();
 
@@ -42,4 +69,5 @@ app.Run();
 // IMPORTANT
 // From Bookify root
 // To create a new migration, run the following command:
-// dotnet ef migrations add Create_Database --project .\Bookify.Infrastructure\Bookify.Infrastructure.csproj --startup-project .\Bookify.Api\Bookify.Api.csproj
+// dotnet ef migrations add Create_Database --project .\Bookify.Infrastructure\Bookify.Infrastructure.csproj
+// --startup-project .\Bookify.Api\Bookify.Api.csproj
--- Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
+++ Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -1,6 +1,7 @@
-using Bookify.Application.Abstractions.Messaging;
+using Bookify.Domain.Abstractions;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Serilog.Context;
 
 namespace Bookify.Application.Abstractions.Behaviors;
 
@@ -9,50 +10,97 @@ namespace Bookify.Application.Abstractions.Behaviors;
 // We are doing this because, we want to be running logging for our command pipeline. We don't really care about
 // logging our queris, we want our queries to be as fast as possible and return the response to the user straight away,
 // while for commands there is a lot of value in adding some additional information through logging. 
-public class LoggingBehavior<TRequest, TResponse>
+public class LoggingBehavior <TRequest, TResponse>
 	: IPipelineBehavior<TRequest, TResponse>
-	where TRequest : IBaseCommand
+//	where TRequest : IBaseCommand
+	// If we want to update it to run for all of 
+// our mediator requests, which are commands and queries, we can replace this generic constraint
+// to target the base request because our command and query abstractions implement
+// this interface under the hood. 
+	where TRequest : IBaseRequest
+	//  Also we are adding one  more generic constraint that the response type  of our
+	// requests is always a result  object and we  made sure of this when we were defining our 
+	// command and query abstractions. 
+	where TResponse : Result
 {
-	private readonly ILogger<TRequest> _logger;
+//	private readonly ILogger<TRequest> _logger;
 
-	public LoggingBehavior(ILogger<TRequest> logger)
-	{
-		_logger = logger;
-	}
+	private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
 
-	public async Task<TResponse> Handle(
-		TRequest request,
-		RequestHandlerDelegate<TResponse> next,
-		CancellationToken cancellationToken)
+//	public LoggingBehavior(ILogger<TRequest> logger)
+//	{
+//		_logger = logger;
+//	}
+
+	public LoggingBehavior ( ILogger<LoggingBehavior<TRequest, TResponse>> logger ) { _logger = logger; }
+
+	public async Task<TResponse> Handle ( TRequest request,
+										  RequestHandlerDelegate<TResponse> next,
+										  CancellationToken cancellationToken )
 	{
 		// We are using reflection to get the type of the current request, and we are taking the
 		// name of this type, which is going to be our command name. 
-		var name = request.GetType().Name;
+		var name = request.GetType().
+			Name;
 
 		// We wrapp all our logic in a try and catch statement
+		// Logging > The reason of doing thing up (adding generic constraints) is to improve
+		// the logging in our handle method. 
 		try
 		{
 			// Loggin an infromation statement saying that we are executing the command with a particular name
-			_logger.LogInformation("Executing command {Command}", name);
+//			_logger.LogInformation("Executing command {Command}", name);
+			_logger.LogInformation (
+					message : "Executing request {Request}",
+					name
+				);
 
 			// After that we run our request handler delegate, which is our command handler,
 			// we obtain a result instance, and we add another information log
 			var result = await next();
 
+			if ( result.IsSuccess )
+				_logger.LogInformation (
+						message : "Request {Request} processed successfully",
+						name
+					);
+			else
+			{
+//				_logger.LogError ("Request {Request}  processed with {@Error}", name, result.Error  );
+				using ( LogContext.PushProperty (
+							   name : "Error",
+							   value : result.Error,
+							   destructureObjects : true
+						   ) )
+					_logger.LogError (
+							message : "Request {Request} processed with error",
+							name
+						);
+			}
+
 			// That this command was processed successfully
-			_logger.LogInformation("Command {Command} processed successfully", name);
+//			_logger.LogInformation("Command {Command} processed successfully", name);
+//			_logger.LogInformation (
+//					message : "Request {Request} processed successfully",
+//					name
+//				);
 
 			// And in the end, we just return this result from our pipeline, 
 			return result;
 		}
-		catch (Exception exception)
+		catch ( Exception exception )
 		{
 			// And if exception occurs we are going to log an error passing in the
 			// exception and saying that this command processing failed.
 			// We can enrich these logs further with more contextual information like who is the
 			// current user, what is the ID of the command, if we assign IDs to commands, what is correlation ID
 			// or the request ID and so on. 
-			_logger.LogError(exception, "Command {Command} processing failed", name);
+//			_logger.LogError(exception, "Command {Command} processing failed", name);
+			_logger.LogError (
+					exception : exception,
+					message : "Request {Request} processing failed",
+					name
+				);
 
 			throw;
 		}
